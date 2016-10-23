7cd1090a21c0facc0771550272ba695a4bdf5e4e
nordsmallcountry <nordsmallcountry@gmail.com>
2016-10-23 22:44:25 +0300

    пофиксил

---
+++ APITest/APITest/FirstViewController.h
@@ -0,0 +1,14 @@
+//
+//  FirstViewController.h
+//  APITest
+//
+//  Created by Антон  Смирнов on 23.10.16.
+//  Copyright © 2016 Антон  Смирнов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+- (IBAction)didTapeBringNextPageBtn:(id)sender;
+
+@end
---
+++ APITest/APITest/FirstViewController.m
@@ -0,0 +1,42 @@
+//
+//  FirstViewController.m
+//  APITest
+//
+//  Created by Антон  Смирнов on 23.10.16.
+//  Copyright © 2016 Антон  Смирнов. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.navigationItem.title = @"Sign In";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)didTapeBringNextPageBtn:(id)sender {
+    
+    [self performSegueWithIdentifier:@"SequeToNextPage" sender:self];
+}
+@end
